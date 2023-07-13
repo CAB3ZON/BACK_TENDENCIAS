5700b392910d827f5cc4580f708637a08dd6fbf5
CabreraOscar <oscar.cabrera.est@tecazuay.edu.ec>
2023-07-12 23:59:32 -0500

    cambios de tu papi  pelucheee v3

--- src/main/java/com/lavanderia/Lavanderia/LavanderiaApplication.java
+++ src/main/java/com/lavanderia/Lavanderia/LavanderiaApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @SpringBootApplication
+
 public class LavanderiaApplication {
 
 	public static void main(String[] args) {
